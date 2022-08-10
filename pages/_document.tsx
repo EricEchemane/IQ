36a714e6bbcf66e42fb3dc8c6058ba97c4ef12a8
Eric Echemane <eechemane29@gmail.com>
2022-08-10 13:13:11 +0800

    custom document:

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="/favicon.png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
