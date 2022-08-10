1d473f7645c6f55558edb98a78e944b9c1fade35
Eric Echemane <eechemane29@gmail.com>
2022-08-10 14:19:58 +0800

    google auth added

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
+    ],
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            return '/';
+        },
+    }
+}); 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (!session) {
+    router.replace('/login');
+  }
+
+  console.log(session);
+
+
   return (
     <>
       <Head> <title>IQ</title> </Head>
-      <div>IQ</div>
+      <h1> {session?.user?.email} </h1>
     </>
   );
 };
