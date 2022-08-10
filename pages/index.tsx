9340964fd51a2b98e7e9c297b10ee0afa8a13c12
Eric Echemane <eechemane29@gmail.com>
2022-08-10 14:28:19 +0800

    updated auth logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   const router = useRouter();
 
-  if (!session) {
-    router.replace('/login');
-  }
+  useEffect(() => {
+    if (!session) {
+      router.replace('/login');
+    }
+  }, [router, session]);
 
   console.log(session);
 
@@ -18,6 +21,7 @@ const Home: NextPage = () => {
     <>
       <Head> <title>IQ</title> </Head>
       <h1> {session?.user?.email} </h1>
+      <button onClick={() => signOut()}> Sign out </button>
     </>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,11 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Login() {
     const { data: session } = useSession();
+    const router = useRouter();
     if (session) {
-        return (
-            <>
-                Signed in as {session.user?.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        );
+        router.replace('/');
     }
     return (
         <>
