bd64e41d591ba373addc4cc4098596a8945504c7
Eric Echemane <eechemane29@gmail.com>
2022-08-10 19:46:17 +0800

    new entity - question

---
+++ lib/entities/question.entity.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, 'Question is required'],
+        minlength: [5, 'Question must be at least 3 characters long'],
+    },
+    timer: { // in seconds: default to inherit from quiz
+        type: Number || 'inherit',
+        default: 'inherit',
+    },
+    choices: {
+        type: [String],
+        required: [true, 'Choices are required'],
+        validate: {
+            validator: (value: [String]) => {
+                return value.length >= 2;
+            },
+            message: 'Requires atleast two choices',
+        }
+    },
+    correct_choice: {
+        type: String,
+        required: [true, 'Correct choice is required'],
+    },
+    points: {
+        type: Number,
+        default: 1,
+        min: [1, 'Points must be at least 1'],
+    }
+});
+
+export default mongoose.model("Question", questionSchema);
