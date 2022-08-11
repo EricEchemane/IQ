21b72fcfae795c52b1df8135f2a3ea9d8e7847b8
Eric Echemane <eechemane29@gmail.com>
2022-08-11 11:34:32 +0800

    register ui fixed

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,9 @@
+import { Box, Button, Group, PasswordInput, Radio, Stack, TextInput, Title } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Register() {
     const router = useRouter();
@@ -11,28 +13,79 @@ export default function Register() {
             router.replace('/signin');
         }
     });
+    const form = useForm({
+        initialValues: {
+            email: session?.user?.email,
+            type: 'student',
+            course: '',
+            section: '',
+            adminPasscode: ''
+        },
+    });
+
+    useEffect(() => {
+        form.setValues(values => ({
+            ...values,
+            email: session?.user?.email
+        }));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [session?.user?.email]);
 
     return (
         <>
             <Head> <title> Register | Ayq </title> </Head>
-            <form>
-                <input type="email" disabled defaultValue={session?.user?.email || ''} placeholder='email' />
-                <div>
-                    <label htmlFor="student">Student</label>
-                    <input type="radio" name='userType' id='student' />
-                </div>
-                <div>
-                    <label htmlFor="professor">Professor</label>
-                    <input type="radio" name='userType' id='professor' />
-                </div>
-                <div id='for-student'>
-                    <input type="text" placeholder='Course' />
-                    <input type="text" placeholder='Section' />
-                </div>
-                <div id='for-professor'>
-                    <input type="text" placeholder='Admin passcode' />
-                </div>
-            </form>
+            <Stack sx={{ maxWidth: 300 }} mx="auto" my='4rem'>
+                <Title align='center' order={2}> Register </Title>
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <TextInput
+                        label={session?.user?.name || 'Email'}
+                        placeholder="your@email.com"
+                        disabled
+                        defaultValue={session?.user?.email || ''}
+                    />
+
+                    <Radio.Group
+                        value={form.values.type}
+                        onChange={(value) => {
+                            form.setValues({
+                                ...form.values,
+                                type: value
+                            });
+                        }}
+                        label="Your role"
+                        required
+                        my='1rem'
+                    >
+                        <Radio value="student" label="Student" />
+                        <Radio value="professor" label="Professor" />
+                    </Radio.Group>
+
+                    {form.values.type === 'student' && <Box>
+                        <TextInput
+                            required
+                            label='Course'
+                            {...form.getInputProps('course')}
+                        />
+                        <TextInput
+                            required
+                            label='Section'
+                            {...form.getInputProps('section')}
+                        />
+                    </Box>}
+
+                    {form.values.type === 'professor' && <Box>
+                        <PasswordInput
+                            required
+                            label='Admin passcode'
+                            {...form.getInputProps('adminPasscode')}
+                        />
+                    </Box>}
+
+                    <Group position="right" mt="md">
+                        <Button type="submit">Submit</Button>
+                    </Group>
+                </form>
+            </Stack>
         </>
     );
 }
