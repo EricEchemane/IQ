213a517c51774f0f99953e4b77d6bb4e2936c32c
Eric Echemane <eechemane29@gmail.com>
2022-08-11 11:11:06 +0800

    mantine setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 }
