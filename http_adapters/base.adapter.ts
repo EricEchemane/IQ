aa670b1019f39bed8604a155e335d0183af1c3b6
Eric Echemane <eechemane29@gmail.com>
2022-08-13 21:11:42 +0800

    adapter for login created

--- http_adapters/base.adapter.ts
+++ http_adapters/base.adapter.ts
@@ -2,10 +2,22 @@ export default class HttpAdapter {
     url: string;
     payload: any;
     method: string;
+    headers: any;
 
-    constructor(url: string, payload: any, method: string) {
-        this.url = url;
-        this.payload = payload;
-        this.method = method;
+    constructor(params: {
+        url: string;
+        payload: any;
+        method: string;
+        headers?: any;
+        stringiyfyPayload?: true;
+    }) {
+        this.url = params.url;
+        this.method = params.method;
+        this.headers = params.headers;
+
+        if (params.stringiyfyPayload) {
+            this.payload = JSON.stringify(params.payload);
+        }
+        else this.payload = params.payload;
     }
 }
--- http_adapters/user.adapter.ts
+++ http_adapters/user.adapter.ts
@@ -1,5 +1,12 @@
-
+import HttpAdapter from "./base.adapter";
 
 export default class UserAdapter {
 
+    static login = (payload: { email: string; }) => {
+        return new HttpAdapter({
+            url: "/api/user/login",
+            payload,
+            method: "POST",
+        });
+    };
 }
