7b91d057d15dede8ae20de4aafdc5ab14aa6aefc
Eric Echemane <eechemane29@gmail.com>
2022-08-15 22:22:06 +0800

    AddQuestion component

---
+++ components/professor/contents/AddQuestions.tsx
@@ -0,0 +1,12 @@
+import { IQuiz } from 'entities/quiz.entity';
+import React from 'react';
+
+export default function AddQuestions({ onSave, quiz }: {
+    quiz: IQuiz;
+    onSave: Function;
+}) {
+
+    return (
+        <div>AddQuestions</div>
+    );
+}
