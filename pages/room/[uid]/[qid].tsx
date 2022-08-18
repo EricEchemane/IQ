9dcb3d6199e00fcd4f295921dfbb00c0bf210ea8
Eric Echemane <eechemane29@gmail.com>
2022-08-18 21:55:34 +0800

    wrap arround container component

--- pages/room/[uid]/[qid].tsx
+++ pages/room/[uid]/[qid].tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, CopyButton, Divider, Group, Loader, Paper, Stack, Text, Title } from '@mantine/core';
+import { Avatar, Button, Container, CopyButton, Divider, Group, Loader, Paper, Stack, Text, Title } from '@mantine/core';
 import { IconClipboard, IconClipboardCheck, IconRocket } from '@tabler/icons';
 import connectToDatabase from 'db/connectToDatabase';
 import { IQuiz } from 'entities/quiz.entity';
@@ -32,65 +32,69 @@ export default function QuizRoom({ user, quiz }: {
         <Head> <title> Quiz Room - IQ </title> </Head>
 
         {/* HEADER */}
-        <Paper p='sm' shadow={'sm'}>
-            <Group align={'center'} position='apart'>
-                <Group align={'center'}>
-                    <Avatar src={user.image} alt={`profile of ${user.name}`} radius={50} />
-                    <Stack spacing={0}>
-                        <Title order={6}> Prof. {user.name} </Title>
-                        <Text size='sm'> {user.email} </Text>
-                    </Stack>
+        <Paper shadow={'sm'}>
+            <Container p='sm'>
+                <Group align={'center'} position='apart'>
+                    <Group align={'center'}>
+                        <Avatar src={user.image} alt={`profile of ${user.name}`} radius={50} />
+                        <Stack spacing={0}>
+                            <Title order={6}> Prof. {user.name} </Title>
+                            <Text size='sm'> {user.email} </Text>
+                        </Stack>
+                    </Group>
+                    <Group spacing={5}>
+                        <Text color='dimmed'> Quiz Code: </Text>
+                        <CopyButton value={parseRoomId(quiz._id)}>
+                            {({ copied, copy }) => (
+                                <Button
+                                    rightIcon={copied ? <IconClipboardCheck /> : <IconClipboard />}
+                                    radius={50}
+                                    variant='subtle'
+                                    color={copied ? 'teal' : 'blue'}
+                                    onClick={copy}>
+                                    {parseRoomId(quiz._id)}
+                                </Button>
+                            )}
+                        </CopyButton>
+                    </Group>
                 </Group>
-                <Group spacing={5}>
-                    <Text color='dimmed'> Quiz Code: </Text>
-                    <CopyButton value={parseRoomId(quiz._id)}>
-                        {({ copied, copy }) => (
-                            <Button
-                                rightIcon={copied ? <IconClipboardCheck /> : <IconClipboard />}
-                                radius={50}
-                                variant='subtle'
-                                color={copied ? 'teal' : 'blue'}
-                                onClick={copy}>
-                                {parseRoomId(quiz._id)}
-                            </Button>
-                        )}
-                    </CopyButton>
-                </Group>
-            </Group>
+            </Container>
         </Paper>
 
-        <Group align={'center'} position='apart' mt='sm' p='md'>
-            <Stack spacing={4}>
-                <Title order={3}>
-                    {quiz.title}
-                </Title>
-                <Group>
-                    <Text>
-                        {participants.length} {participants.length === 1 ? 'Participant' : 'Participants'}
-                    </Text>
-                    <Divider size={'sm'} orientation='vertical' />
-                    <Text> {quiz.questions.length} Questions </Text>
-                </Group>
+        <Container p='md'>
+            <Group align={'center'} position='apart' mt='sm'>
+                <Stack spacing={4}>
+                    <Title order={3}>
+                        {quiz.title}
+                    </Title>
+                    <Group>
+                        <Text>
+                            {participants.length} {participants.length === 1 ? 'Participant' : 'Participants'}
+                        </Text>
+                        <Divider size={'sm'} orientation='vertical' />
+                        <Text> {quiz.questions.length} Questions </Text>
+                    </Group>
+                </Stack>
+
+                <Button
+                    disabled={participants.length === 0}
+                    rightIcon={<IconRocket strokeWidth={1.5} />}
+                    size='md'> Start </Button>
+            </Group>
+
+            <Stack align='center' p='md' mt='3rem'>
+                <Loader size="xl" variant="bars" />
+                <Text
+                    align='center'
+                    weight={'bold'}
+                    size='xl'
+                    color='dimmed'> Waiting for other participants to join </Text>
+                <Button
+                    onClick={cancelQuiz}
+                    variant='light'
+                    mt='xl'> Cancel and return home </Button>
             </Stack>
-
-            <Button
-                disabled={participants.length === 0}
-                rightIcon={<IconRocket strokeWidth={1.5} />}
-                size='md'> Start </Button>
-        </Group>
-
-        <Stack align='center' p='md' mt='3rem'>
-            <Loader size="xl" variant="bars" />
-            <Text
-                align='center'
-                weight={'bold'}
-                size='xl'
-                color='dimmed'> Waiting for other participants to join </Text>
-            <Button
-                onClick={cancelQuiz}
-                variant='light'
-                mt='xl'> Cancel and return home </Button>
-        </Stack>
+        </Container>
     </>;
 }
 
