d4705d037893acb472587f771c1ce25c972fd691
Eric Echemane <eechemane29@gmail.com>
2022-08-19 17:23:35 +0800

    remove message handler file

--- lib/socket/messageHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Server, Socket } from 'socket.io';
-
-export default function messageHandler(io: Server, socket: Socket) {
-
-    socket.on("createdMessage", (msg: any) => {
-        socket.broadcast.emit("newIncomingMessage", msg);
-    });
-};
