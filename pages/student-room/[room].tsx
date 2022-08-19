e18dfb3949b89ecb4aa6d6ce9a6f75224adc96d5
Eric Echemane <eechemane29@gmail.com>
2022-08-19 09:14:49 +0800

    added getServerSideProps for getting user info

--- pages/student-room/[room].tsx
+++ pages/student-room/[room].tsx
@@ -1,7 +1,42 @@
+import connectToDatabase from 'db/connectToDatabase';
+import { IUser } from 'entities/user.entity';
+import { GetServerSideProps } from 'next';
+import { getToken } from 'next-auth/jwt';
+import { useRouter } from 'next/router';
 import React from 'react';
 
-export default function StudentRoom() {
-    return (
-        <div>[room]</div>
-    );
+export default function StudentRoom({ user }: { user: IUser; }) {
+    const router = useRouter();
+
+    return <>
+
+    </>;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const redirectObject = {
+        redirect: {
+            permanent: false,
+            destination: '/'
+        }
+    };
+
+    try {
+        const token = await getToken({ req: context.req });
+        if (!token) return redirectObject;
+
+        const db = await connectToDatabase();
+        if (!db) return redirectObject;
+
+        const user = await db.models.User.findOne({ email: token.email });
+        if (!user) return redirectObject;
+
+        return {
+            props: {
+                user: JSON.parse(JSON.stringify(user))
+            }
+        };
+    } catch (error) {
+        return redirectObject;
+    }
+};
