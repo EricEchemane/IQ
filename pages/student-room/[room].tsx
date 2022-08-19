256ac0853acf03dd8ed062730430e09cd559e4b2
Eric Echemane <eechemane29@gmail.com>
2022-08-19 09:35:19 +0800

    header added to student room

--- pages/student-room/[room].tsx
+++ pages/student-room/[room].tsx
@@ -1,49 +1,96 @@
-import { Container, Paper } from '@mantine/core';
+import { Avatar, Container, Divider, Group, Indicator, Paper, Stack, Text, Title } from '@mantine/core';
 import connectToDatabase from 'db/connectToDatabase';
 import { IUser } from 'entities/user.entity';
+import { StudentEvents } from 'lib/quiz_room/types';
 import { GetServerSideProps } from 'next';
 import { getToken } from 'next-auth/jwt';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import io, { Socket } from "socket.io-client";
+
+let socket: Socket;
 
 export default function StudentRoom({ user }: { user: IUser; }) {
     const router = useRouter();
     const { room } = router.query;
+    const [connected, setConnected] = useState(false);
+
+    const socketInitializer = useCallback(async () => {
+        if (socket) return;
+        await fetch("/api/room");
+        socket = io();
+        socket.on('connect', () => {
+            setConnected(true);
+        });
+    }, []);
+
+    useEffect(() => {
+        socketInitializer();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return <>
-        <Head> <title> Quiz Room - {room} </title> </Head>
+        <Head> <title> Student Room | IQ </title> </Head>
 
-        <Container p='sm'>
-            <Paper>  </Paper>
-        </Container>
+        <Paper shadow={'sm'}>
+            <Container p='sm'>
+                <Group align={'center'} position='apart'>
+                    <Group align={'center'}>
+                        {connected
+                            ? <Indicator color="green" size={15} withBorder>
+                                <Avatar src={user.image} alt={`profile of ${user.name}`} radius={10} />
+                            </Indicator>
+                            : <Avatar src={user.image} alt={`profile of ${user.name}`} radius={10} />}
+                        <Stack spacing={0}>
+                            <Title order={6}> {user.name} </Title>
+                            <Group>
+                                <Text size='sm'> {user.email} </Text>
+                                <Divider orientation='vertical' />
+                                <Text size='sm'>
+                                    {user.course.toUpperCase()} {user.section.toUpperCase()}
+                                </Text>
+                            </Group>
+                        </Stack>
+                    </Group>
+                    <Group spacing={5}>
+                        {/* <Text color='dimmed'> Quiz Code: </Text> */}
+                    </Group>
+                </Group>
+            </Container>
+        </Paper>
     </>;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const redirectObject = {
-        redirect: {
-            permanent: false,
-            destination: '/'
-        }
-    };
-
     try {
         const token = await getToken({ req: context.req });
-        if (!token) return redirectObject;
-
         const db = await connectToDatabase();
-        if (!db) return redirectObject;
 
-        const user = await db.models.User.findOne({ email: token.email });
-        if (!user) return redirectObject;
-
-        return {
-            props: {
-                user: JSON.parse(JSON.stringify(user))
+        const user: any = await db?.models.User.findOne({ email: token?.email });
+        if (!user) return {
+            redirect: {
+                permanent: false,
+                destination: '/'
             }
         };
+
+        if (user.type !== 'student') return {
+            redirect: {
+                permanent: false,
+                destination: '/'
+            }
+        };
+
+        return {
+            props: { user: JSON.parse(JSON.stringify(user)) }
+        };
     } catch (error) {
-        return redirectObject;
+        return {
+            redirect: {
+                permanent: false,
+                destination: '/'
+            }
+        };
     }
 };
