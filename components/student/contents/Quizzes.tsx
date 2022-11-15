fa90cd53cb7081f7cb4397048533a647f1764a19
Eric Echemane <eechemane29@gmail.com>
2022-11-15 22:25:06 +0800

    added view answers for reviewer

--- components/student/contents/Quizzes.tsx
+++ components/student/contents/Quizzes.tsx
@@ -1,10 +1,22 @@
-import { Divider, Group, Paper, Stack, Text, Title } from '@mantine/core';
+import { Button, Divider, Group, Modal, Paper, Stack, Text, Title } from '@mantine/core';
 import moment from 'moment';
-import React from 'react';
+import React, { useState } from 'react';
+
+interface SelectedQuiz {
+    title: string;
+    answers: string[];
+    questions: {
+        choices: string[];
+        correct_choice: string;
+        question: string;
+    }[];
+}
 
 export default function Quizzes(props: {
     quizzes: any[];
 }) {
+    const [selectedQuiz, setSelectedQuiz] = useState<SelectedQuiz | undefined>();
+
     return <>
         <Title order={3}> Your Quizzes </Title>
         <Stack my='lg'>
@@ -20,6 +32,17 @@ export default function Quizzes(props: {
                             <Text> Final Score: {data.final_score} - Rank: {data.ranking} </Text>
                             <Divider orientation='vertical' />
                             <Text> Taken on: {moment(data.date_finished).format('LL')} </Text>
+                            <Divider orientation='vertical' />
+                            <Button
+                                onClick={() => setSelectedQuiz({
+                                    answers: data.answers,
+                                    questions: data.quiz.questions,
+                                    question: data.quiz.question,
+                                    title: data.quiz.title,
+                                } as any)}
+                                variant='light'>
+                                my answers
+                            </Button>
                         </Group>
                     </Stack>
                 </Paper>
@@ -28,5 +51,54 @@ export default function Quizzes(props: {
                 <Text> You have not taken any quiz yet </Text>
             </>}
         </Stack>
+
+        <Modal
+            size={'lg'}
+            opened={!!selectedQuiz}
+            onClose={() => setSelectedQuiz(undefined)}
+            title={"Your answers in " + selectedQuiz?.title}
+        >
+            {
+                selectedQuiz?.questions.map((q, index) => {
+                    const isCorrect = q.correct_choice === selectedQuiz.answers[index];
+                    return <QuizItemAnswer
+                        itemNumber={index + 1}
+                        correct_answer={q.correct_choice}
+                        answer={selectedQuiz.answers[index]}
+                        question={q.question}
+                        key={index}
+                        isCorrect={isCorrect} />;
+                })
+            }
+        </Modal>
     </>;
 }
+
+type QuizItemAnswerProps = {
+    isCorrect: boolean;
+    question: string;
+    answer: string;
+    correct_answer: string;
+    itemNumber: number;
+};
+
+const QuizItemAnswer = ({
+    isCorrect,
+    answer,
+    correct_answer,
+    question,
+    itemNumber
+}: QuizItemAnswerProps) => {
+    return <>
+        <Stack mb={'xl'}>
+            <Title order={4}> {itemNumber}. {question} </Title>
+            <Text>
+                You answered: <b> {answer} </b> {isCorrect && '✅correct'}
+            </Text>
+            <Text>
+                {!isCorrect && '❌wrong, the correct answer is ' + correct_answer}
+            </Text>
+        </Stack>
+        <Divider />
+    </>;
+};
